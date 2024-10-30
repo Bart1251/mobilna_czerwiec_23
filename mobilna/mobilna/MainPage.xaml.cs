f2d6c8f8e7632b0394e5de9a8e9129bb6436389c
Bartłomiej Więcek <bartlomiej.wiecek@vp.pl>
2024-10-30 07:41:15 +0100

    Dodaj pliki projektów.

---
+++ mobilna/mobilna/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mobilna
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] Quotes = { "Dzień dobry", "Good morning", "Buenos dias" };
+        int i = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeQuote(object sender, EventArgs e)
+        {
+            i = (i + 1) % Quotes.Length;
+            Quote.Text = Quotes[i];
+        }
+    }
+}
